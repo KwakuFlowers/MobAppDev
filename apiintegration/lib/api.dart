f43a7edd3814c3fdb92717a0a408d00386815081
KwakuFlowers <kwakuflowers@gmail.com>
2022-07-27 23:26:07 -0400

    disgrace of an attempt at API extra credit

---
+++ apiintegration/lib/api.dart
@@ -0,0 +1,9 @@
+import 'dart:html';
+
+import 'package:apiintegration/main.dart';
+import ""
+
+class APIint {
+  final api_url_artist = "https://api.spotify.com/v1/artists/";
+  final api_token_url = "https://accounts.spotify.com/api/token";
+}
